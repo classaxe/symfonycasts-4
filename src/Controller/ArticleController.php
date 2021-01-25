7c2fa85d4f2b354b0b5c08ce0b8e2f9a60950835
Martin Francis <martin@classaxe.com>
2021-01-24 22:24:56 -0500

    symfony4-1 6.0

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,10 +1,11 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -17,10 +18,17 @@ class ArticleController
     /**
      * @Route("/news/{slug}")
      */
-    public function news($slug)
+    public function show($slug)
     {
-        return new Response(
-            sprintf('Hello %s', $slug)
-        );
+        $comments = [
+            'Commemnt 1 and so on',
+            'Comment B the second of it',
+            'The  third comment goes here'
+        ];
+        $title = ucwords(str_replace('-', ' ', $slug));
+        return $this->render('article/show.html.twig', [
+            'comments' =>   $comments,
+            'title' =>      $title
+        ]);
     }
 }
