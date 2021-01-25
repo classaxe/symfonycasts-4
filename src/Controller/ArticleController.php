b63d7eccf86502c11bb6d6884c9ca07b3b46d8d8
Martin Francis <martin@classaxe.com>
2021-01-25 11:24:45 -0500

    symfony4-2 2.0 Caching

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use Michelf\MarkdownInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,7 +22,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug, MarkdownInterface $markdown)
+    public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache)
     {
         $comments = [
             'Commemnt 1 and so on',
@@ -32,7 +33,7 @@ class ArticleController extends AbstractController
 Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
 lorem proident [beef ribs](https://baconipsum.com) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
 labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
-turkey shank eu pork belly meatball non cupim.
+**turkey** shank eu pork belly meatball non cupim.
 
 Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
 laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
@@ -58,11 +59,15 @@ belly tongue alcatra, shoulder excepteur in beef bresaola duis ham bacon eiusmod
 adipisicing cow cillum tenderloin.
 
 EOF;
-
-
+        $item = $cache->getItem('markdown_' .md5($articleContent));
+        if (!$item->isHit()) {
+            $item->set($markdown->transform($articleContent));
+            $cache->save($item);
+        }
+        $articleContent = $item->get();
         $title = ucwords(str_replace('-', ' ', $slug));
         return $this->render('article/show.html.twig', [
-            'articleContent' => $markdown->transform($articleContent),
+            'articleContent' => $articleContent,
             'comments' =>       $comments,
             'slug' =>           $slug,
             'title' =>          $title
