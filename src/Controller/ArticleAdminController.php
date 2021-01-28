ceb904733a0d19c3429272a675e90d317f81f9eb
Martin Francis <martin@classaxe.com>
2021-01-28 11:54:34 -0500

    symfony4-3 14.0 Using fixtures to populate test data

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -4,8 +4,6 @@
 namespace App\Controller;
 
 
-use App\Entity\Article;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,68 +13,8 @@ class ArticleAdminController extends AbstractController
     /**
      * @Route("/admin/article/new")
      */
-    public function new(EntityManagerInterface $em)
+    public function new()
     {
-        $article = new Article();
-        $article
-            ->setTitle('Why Asteroids Taste Like Bacon')
-            ->setSlug('why-asteroids-taste-like-bacon-'.random_int(100,999))
-            ->setContent(<<< EOF
-Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
-lorem proident [beef ribs](https://baconipsum.com) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
-labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
-**turkey** shank eu pork belly meatball non cupim.
-
-Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
-laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
-capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
-picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
-occaecat lorem meatball prosciutto quis strip steak.
-
-Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
-mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
-strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
-cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
-fugiat.
-
-Sausage tenderloin officia jerky nostrud. Laborum elit pastrami non, pig kevin buffalo minim ex quis. Pork belly
-pork chop officia anim. Irure tempor leberkas kevin adipisicing cupidatat qui buffalo ham aliqua pork belly
-exercitation eiusmod. Exercitation incididunt rump laborum, t-bone short ribs buffalo ut shankle pork chop
-bresaola shoulder burgdoggen fugiat. Adipisicing nostrud chicken consequat beef ribs, quis filet mignon do.
-Prosciutto capicola mollit shankle aliquip do dolore hamburger brisket turducken eu.
-
-Do mollit deserunt prosciutto laborum. Duis sint tongue quis nisi. Capicola qui beef ribs dolore pariatur.
-Minim strip steak fugiat nisi est, meatloaf pig aute. Swine rump turducken nulla sausage. Reprehenderit pork
-belly tongue alcatra, shoulder excepteur in beef bresaola duis ham bacon eiusmod. Doner drumstick short loin,
-adipisicing cow cillum tenderloin.
-
-EOF
-            );
-        if (rand(1,10) > 2) {
-            $article->setPublishedAt(new \DateTime(sprintf('-%d days', rand(1,100))));
-        }
-        $images = [
-            'alien-profile.png',
-            'asteroid.jpeg',
-            'astronaut-profile.png',
-            'lightspeed.png',
-            'mercury.jpeg',
-            'meteor-shower.jpg',
-            'space-ice.png',
-            'space-nav.jpg'
-        ];
-        $article->setAuthor('Mike Ferengi')
-            ->setHeartCount(rand(0, 100))
-            ->setImageFilename($images[rand(0, count($images) -1)]);
-
-        $em->persist($article);
-        $em->flush();
-
-        return new Response(
-            sprintf(
-                'New article id: %d, slug: %s',
-                $article->getId(),
-                $article->getSlug()
-            ));
+        return new Response('To Do');
     }
 }
---
+++ src/DataFixtures/BaseFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\DataFixtures;
+
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+abstract class BaseFixtures extends Fixture
+{
+    /** @var ObjectManager */
+    private $manager;
+
+    abstract protected function loadData(ObjectManager $em);
+
+    public function load(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+        $this->loadData($manager);
+    }
+
+    protected function createMany(string $className, int $count, callable $factory)
+    {
+        for ($i = 0; $i < $count; $i++) {
+            $entity = new $className();
+            $factory($entity, $i);
+
+            $this->manager->persist($entity);
+            // store for usage later as App\Entity\ClassName_#COUNT#
+            $this->addReference($className . '_' . $i, $entity);
+        }
+    }
+}
