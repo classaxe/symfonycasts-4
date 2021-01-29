8dd4a618a7438d88a7640fca5b903e51abb48800
Martin Francis <martin@classaxe.com>
2021-01-29 12:34:47 -0500

    symfony4-4 12 Twig extensions (mine uses 'composer require twig/string-extra')

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,11 @@ class CommentAdminController extends AbstractController
     /**
      * @Route("/admin/comment", name="comment_admin")
      */
-    public function index(): Response
+    public function index(CommentRepository $repository): Response
     {
+        $comments = $repository->findBy([], ['createdAt' => 'DESC']);
         return $this->render('comment_admin/index.html.twig', [
-            'controller_name' => 'CommentAdminController',
+            'comments' => $comments,
         ]);
     }
 }
