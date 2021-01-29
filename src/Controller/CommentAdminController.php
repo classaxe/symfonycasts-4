ce9fe3fbb1a69dce762da4675199d6a7fd56824e
Martin Francis <martin@classaxe.com>
2021-01-29 12:54:15 -0500

    symfony4-4 13 Request Object & Query OR Logic

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,9 +13,13 @@ class CommentAdminController extends AbstractController
     /**
      * @Route("/admin/comment", name="comment_admin")
      */
-    public function index(CommentRepository $repository): Response
+    public function index(
+        CommentRepository $repository,
+        Request $request
+    ): Response
     {
-        $comments = $repository->findBy([], ['createdAt' => 'DESC']);
+        $q = $request->query->get('q');
+        $comments = $repository->findAllWithSearch($q);
         return $this->render('comment_admin/index.html.twig', [
             'comments' => $comments,
         ]);
