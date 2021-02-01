a371ddb1bbd0e89ec9b7d5a736be9c6a1bfd107d
Martin Francis <martin@classaxe.com>
2021-02-01 17:30:56 -0500

    symfony4-5 3.0 Adding First Name and making the migration - 5.6 Doctrine type for JSON data

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -28,6 +28,11 @@ class User implements UserInterface
      */
     private $roles = [];
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -97,5 +102,17 @@ class User implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
     }
 }
