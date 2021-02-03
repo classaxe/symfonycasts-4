8defe60558818c162760f42e58aae95436db027d
Martin Francis <martin@classaxe.com>
2021-02-03 14:49:44 -0500

    symfony4-5 5.0 Firewalls & Authenticator

---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+
+class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
+{
+    public function supports(Request $request)
+    {
+        die('our authenticator is alive');
+    }
+
+    public function getCredentials(Request $request)
+    {
+        // todo
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        // todo
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        // todo
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        // todo
+    }
+
+    protected function getLoginUrl()
+    {
+        // TODO: Implement getLoginUrl() method.
+    }
+
+
+}
