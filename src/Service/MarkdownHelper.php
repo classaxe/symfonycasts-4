24a1b0d17114576850c7c48e8f3028db6a98b626
Martin Francis <martin@classaxe.com>
2021-02-05 10:01:22 -0500

    symfony4-5 20.0 Fetching the User In a Service

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -7,30 +7,41 @@ namespace App\Service;
 use Michelf\MarkdownInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Symfony\Component\Security\Core\Security;
 
 class MarkdownHelper
 {
     private $cache;
     private $markdown;
     private $logger;
+    /** @var Security */
+    private $security;
 
     /**
-     * @return mixed
+     * @param MarkdownInterface $markdown
+     * @param AdapterInterface $cache
+     * @param LoggerInterface $markdownLogger
+     * @param Security $security
      */
     public function __construct(
         MarkdownInterface $markdown,
         AdapterInterface $cache,
-        LoggerInterface $markdownLogger
+        LoggerInterface $markdownLogger,
+        Security $security
     ) {
         $this->cache = $cache;
         $this->logger = $markdownLogger;
         $this->markdown = $markdown;
+        $this->security = $security;
     }
 
     public function parse(string $source): string
     {
         if (stripos($source, 'bacon') !== false) {
-            $this->logger->info('They are talking about bacon again!');
+            $this->logger->info(
+                'They are talking about bacon again!',
+                [ 'user' => $this->security->getUser()]
+            );
         }
 
         $item = $this->cache->getItem('markdown_' .md5($source));
