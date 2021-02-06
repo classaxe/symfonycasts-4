98be190cc38eb10de72cde5e0ad4ddf7b9be22e7
Martin Francis <martin@classaxe.com>
2021-02-06 10:43:46 -0500

    symfony4-5 27.0 API Token Authenticator

--- src/Security/ApiTokenAuthenticator.php
+++ src/Security/ApiTokenAuthenticator.php
@@ -2,6 +2,7 @@
 
 namespace App\Security;
 
+use App\Repository\ApiTokenRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
@@ -11,24 +12,40 @@ use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
 
 class ApiTokenAuthenticator extends AbstractGuardAuthenticator
 {
+    /** @var ApiTokenRepository */
+    private $apiTokenRepository;
+
+    public function __construct(ApiTokenRepository $apiTokenRepository)
+    {
+        $this->apiTokenRepository = $apiTokenRepository;
+    }
+
     public function supports(Request $request)
     {
-        // todo
+        return $request->headers->has('Authorization') &&
+            0 === strpos($request->headers->get('Authorization'), 'Bearer ');
     }
 
     public function getCredentials(Request $request)
     {
-        // todo
+        $authorizationHeader = $request->headers->get('Authorization');
+        return substr($authorizationHeader, strlen('Bearer '));
     }
 
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        // todo
+        if (!$token = $this->apiTokenRepository->findOneBy([
+            'token' => $credentials
+        ])) {
+            return null;
+        }
+
+        return $token->getUser();
     }
 
     public function checkCredentials($credentials, UserInterface $user)
     {
-        // todo
+        print "<pre>" . print_r('Checking Creds', true) . "</pre>"; die;
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
