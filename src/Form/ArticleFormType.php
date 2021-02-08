8cb13ae87e8cfc7b65b98f04fe2abd6982809fd0
Martin Francis <martin@classaxe.com>
2021-02-07 20:24:01 -0500

    symfony4-6 5.0 Field Types & Options

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -6,6 +6,8 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,8 +16,10 @@ class ArticleFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('content');
+            ->add('title', TextType::class, [
+                'help' => 'Choose something catchy!'
+            ])
+            ->add('content', TextareaType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
