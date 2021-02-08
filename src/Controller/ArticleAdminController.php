af8de5cffb4319582c170696a2921b0eff47a94e
Martin Francis <martin@classaxe.com>
2021-02-07 19:18:48 -0500

    symfony4-6 3.0 Success (Flash) Messages

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -36,7 +36,9 @@ class ArticleAdminController extends AbstractController
             $em->persist($article);
             $em->flush();
 
-            return $this->redirectToRoute('app_homepage');
+            $this->addFlash('success', 'Article Created');
+
+            return $this->redirectToRoute('admin_article_list');
         }
 
         return $this->render('article_admin/new.html.twig', [
@@ -54,7 +56,10 @@ class ArticleAdminController extends AbstractController
     }
 
     /**
-     * @Route("/admin/article/")
+     * @Route(
+     *     "/admin/article/",
+     *     name="admin_article_list"
+ *     )
      * @IsGranted("ROLE_ADMIN_ARTICLE")
      */
     public function list(ArticleRepository $articleRepo)
