8c51aa60af11afbec6a52cbc4fe2bf8a55bee66f
Martin Francis <martin@classaxe.com>
2021-02-08 18:16:39 -0500

    symfony4-6 15.0 Adding Extra 'Unmapped' Fields

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -58,7 +58,7 @@ class SecurityController extends AbstractController
             $user = $form->getData();
             $passwordEnc = $passwordEncoder->encodePassword(
                 $user,
-                $user->getPassword()
+                $form['plainPassword']->getData()
             );
 
             $user
--- src/Form/UserRegistrationFormType.php
+++ src/Form/UserRegistrationFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,10 @@ class UserRegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('password')
+            // don't use password to avoid risking persisting plaintext password in database
+            ->add('plainPassword', PasswordType::class, [
+                'mapped' => false
+            ])
         ;
     }
 
