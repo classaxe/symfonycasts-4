13c111fda15cb3c8bd6fa1204e2470a477b7ece1
Martin Francis <martin@classaxe.com>
2021-02-08 16:14:19 -0500

    symfony4-6 13.0 The Edit Form

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -43,12 +43,32 @@ class ArticleAdminController extends AbstractController
     }
 
     /**
-     * @Route("/admin/article/{id}/edit")
+     * @Route(
+     *     "/admin/article/{id}/edit",
+     *     name="admin_article_edit"
+     * )
      * @IsGranted("MANAGE", subject="article")
      */
-    public function edit(Article $article)
+    public function edit(Article $article, EntityManagerInterface $em, Request $request)
     {
-        dd($article);
+        $form = $this->createForm(ArticleFormType::class, $article);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var Article $article */
+            $article = $form->getData();
+            $em->persist($article);
+            $em->flush();
+
+            $this->addFlash('success', 'Article Updated');
+
+            return $this->redirectToRoute('admin_article_edit', [
+                'id' => $article->getId()
+            ]);
+        }
+
+        return $this->render('article_admin/edit.html.twig', [
+            'articleForm' => $form->createView(),
+        ]);
     }
 
     /**
