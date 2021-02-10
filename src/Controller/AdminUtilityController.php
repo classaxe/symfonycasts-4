5b5c59d1cc9bbba365cfaaa7e7293e79a0d90be6
Martin Francis <martin@classaxe.com>
2021-02-10 09:17:28 -0500

    symfony4-6 29.0 Hooking up the AJAX Autocomplete

--- src/Controller/AdminUtilityController.php
+++ src/Controller/AdminUtilityController.php
@@ -7,6 +7,7 @@ namespace App\Controller;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminUtilityController extends AbstractController
@@ -14,14 +15,20 @@ class AdminUtilityController extends AbstractController
     /**
      * @Route(
      *     "/admin/utility/users",
-     *     methods="GET"
+     *     methods="GET",
+     *     name="admin_utility_users"
      * )
      * @param UserRepository $userRepository
+     * @param Request $request
      * @return JsonResponse
      */
-    public function getUsersApi(UserRepository $userRepository)
-    {
-        $users = $userRepository->findAllEmailAlphabetical();
+    public function getUsersApi(
+        UserRepository $userRepository,
+        Request $request
+    ) {
+        $users = $userRepository->findAllMatching(
+            $request->query->get('query')
+        );
 
         return $this->json([
             'users' => $users
