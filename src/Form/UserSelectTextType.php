6503c9d5adadce45c5001b27a69f4a4e2912065c
Martin Francis <martin@classaxe.com>
2021-02-10 09:17:28 -0500

    symfony4-6 30.0 The buildView() Method

--- src/Form/UserSelectTextType.php
+++ src/Form/UserSelectTextType.php
@@ -6,6 +6,8 @@ use App\Repository\UserRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Routing\RouterInterface;
 
@@ -40,14 +42,28 @@ class UserSelectTextType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'attr' => [
-                'class' => 'js-user-autocomplete',
-                'data-autocomplete-url' => $this->router->generate('admin_utility_users'),
-            ],
             'finder_callback' => function(UserRepository $userRepository, string $email) {
                 return $userRepository->findOneBy(['email' => $email]);
             },
             'invalid_message' => 'User not found'
         ]);
+    }
+
+    /**
+     * @param FormView $view
+     * @param FormInterface $form
+     * @param array $options
+     */
+    public function buildView(
+        FormView $view,
+        FormInterface $form,
+        array $options
+    ) {
+        $attr = $view->vars['attr'];
+        $class = (isset($attr['class']) ? $attr['class'] . ' ' : '');
+        $class .= 'js-user-autocomplete';
+        $attr['class'] = $class;
+        $attr['data-autocomplete-url'] = $this->router->generate('admin_utility_users');
+        $view->vars['attr'] = $attr;
     }
 }
